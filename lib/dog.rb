420993f71cb5be3183fce58c480a8f248aa3fb39
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-27 15:57:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,15 @@ class Dog
     @name
   end
   
-  def name=
+  def name=(name)
     @name = name
   end
   
   def breed
     @breed
+  end
+  
+  def breed=(breed)
+    @breed = breed
   end
 end
